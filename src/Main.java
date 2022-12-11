822f40b34fa92df9e40e394d0027b3d65107aa42
Lion2517 <lion2517@yahoo>
2022-12-11 16:56:24 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 130; // Вес в кг
+        int growth = 1.8; // Рост в сантиметрах
+        int massEffect = weight / (growth * growth); // Формула расчёта
+
+    }
+}

5f6c22fc7e9e41ef8295086c818091a14050b5a9
AshurMezan <102794363+AshurMezan@users.noreply.github.com>
2022-12-11 17:02:24 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        int weight = 130; // Вес в кг
-        int growth = 1.8; // Рост в сантиметрах
-        int massEffect = weight / (growth * growth); // Формула расчёта
+        int weight = 90; // Вес в кг
+        double growth = 1.8; // Рост в сантиметрах
+        double massEffect = weight / (growth * growth); // Формула расчёта
 
+        if (massEffect < 16.5) {
+            System.out.println("Ваш индекс массы тела: " + massEffect + " У вас выраженный дефицит массы.");
+        } else if (massEffect < 18.5) {
+            System.out.println("Ваш индекс массы тела: " + massEffect + " У вас недостаточная (дефицит) масса тела.");
+        } else if (massEffect < 25) {
+            System.out.println("Ваш индекс массы тела: " + massEffect + " У вас нормальный вес здорового человека.");
+        } else if (massEffect < 30) {
+            System.out.println("Ваш индекс массы тела: " + massEffect + " У вас избыточная масса тела.");
+        } else if (massEffect < 35) {
+            System.out.println("Ваш индекс массы тела: " + massEffect + " У вас ожирение первой степени.");
+        } else if (massEffect < 40) {
+            System.out.println("Ваш индекс массы тела: " + massEffect + " У вас ожирение второй степени.");
+        } else if (massEffect >= 40) {
+            System.out.println("Ваш индекс массы тела: " + massEffect + " У вас ожирение третьей степени.");
+        }
     }
 }
